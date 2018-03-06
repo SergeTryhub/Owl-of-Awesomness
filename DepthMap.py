0f0a54f05c782ca243b0f2bda596de91913c2f38
Callum Moscrip <36271783+CallumMoscrip@users.noreply.github.com>
2018-03-06 11:37:56 +0000

    Add files via upload

--- DepthMap.py
+++ DepthMap.py
@@ -7,21 +7,23 @@ import cv2
 import StringIO
 import time
 import keyboard
+import numpy
+from matplotlib import pyplot as plt
 
 TCP_IP = '10.0.0.10'
 TCP_PORT = 12345
 BUFFER_SIZE = 24
 #s.close()
 
-RxC=1530 
+RxC=1460 
 RyC=1560
-LxC=1420
-LyC=1440 
+LxC=1540
+LyC=1470 
 NeckC=1530
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-#s.send('{} {} {} {} {}'.format(RxC, RyC, LxC, LyC, NeckC))
+s.send('{} {} {} {} {}'.format(RxC, RyC, LxC, LyC, NeckC))
 
 #camera matrix
 LeftMat=[[582.39100314, 0, 299.87247641],
@@ -35,17 +37,48 @@ RightMat=[[590.61725106, 0, 288.20236937],
          [0, 0, 1]]
 
 RightDist=[0.05790892, -0.41533829, -0.00990576, -0.00553869, 3.12468689]
-
-while True:
-    #get image, split and assign to left and right
+count=0
+cap = cv2.VideoCapture('http://10.0.0.10:8080/stream/video.mjpeg')
+if __name__ == '__main__':
     cap = cv2.VideoCapture('http://10.0.0.10:8080/stream/video.mjpeg')
-    ret, img = cap.read()
-    cv2.imshow("image", img)
-    img1 = img[0:480,  0:640]       #left
-    img2 = img[0:480,  640:1280]   #right
-    cv2.waitKey(30)
 
-    stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
-    disparity = stereo.compute(imgL,imgR)
-    plt.imshow(disparity,'gray')
-    plt.show()
+    while True:
+        if not (left.grab() and right.grab()):
+            print("No more frames")
+            break
+
+        _, leftFrame = left.retrieve()
+        _, rightFrame = right.retrieve()
+
+        cv2.imshow('left', leftFrame)
+        cv2.imshow('right', rightFrame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    left.release()
+    right.release()
+    cv2.destroyAllWindows()
+
+        '''
+        #get image, split and assign to left and right
+        ret, img = cap.read()
+        
+        img1 = img[0:480,  0:640]       #left
+        img2 = img[0:480,  640:1280]   #right
+        #cv2.imshow("Left", img1)
+        #cv2.imshow("Right", img2)
+        singleL=cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+        singleR=cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+        cv2.waitKey()
+
+        stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+        disparity = stereo.compute(singleL,singleR)
+        plt.imshow(disparity,'gray')
+        plt.show()
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+        count+=1
+        https://albertarmea.com/post/opencv-stereo-camera/
+    '''
+        
+        
