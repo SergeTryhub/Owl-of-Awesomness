f0e7848c119c285cd19e10f636b4f7471c5c7b98
Callum Moscrip <36271783+CallumMoscrip@users.noreply.github.com>
2018-03-06 18:44:04 +0000

    Add files via upload

--- DepthMap.py
+++ DepthMap.py
@@ -7,7 +7,7 @@ import cv2
 import StringIO
 import time
 import keyboard
-import numpy
+import numpy as np
 from matplotlib import pyplot as plt
 
 TCP_IP = '10.0.0.10'
@@ -38,47 +38,111 @@ RightMat=[[590.61725106, 0, 288.20236937],
 
 RightDist=[0.05790892, -0.41533829, -0.00990576, -0.00553869, 3.12468689]
 count=0
-cap = cv2.VideoCapture('http://10.0.0.10:8080/stream/video.mjpeg')
+
+ply_header = '''ply
+format ascii 1.0
+element vertex %(vert_num)d
+property float x
+property float y
+property float z
+property uchar red
+property uchar green
+property uchar blue
+end_header
+'''
+
+def write_ply(fn, verts, colors):
+    verts = verts.reshape(-1, 3)
+    colors = colors.reshape(-1, 3)
+    verts = np.hstack([verts, colors])
+    with open(fn, 'wb') as f:
+        f.write((ply_header % dict(vert_num=len(verts))).encode('utf-8'))
+        np.savetxt(f, verts, fmt='%f %f %f %d %d %d ')
+
 if __name__ == '__main__':
     cap = cv2.VideoCapture('http://10.0.0.10:8080/stream/video.mjpeg')
+    time.sleep(1)
 
+    
     while True:
-        if not (left.grab() and right.grab()):
-            print("No more frames")
-            break
-
-        _, leftFrame = left.retrieve()
-        _, rightFrame = right.retrieve()
-
+        #print('in loop')
+        
+        ret, Frame = cap.read()
+        imgR = rightFrame = Frame[0:480,  0:640]       #right eye
+        imgL = leftFrame = Frame[0:480,  640:1280]   #left eye
         cv2.imshow('left', leftFrame)
         cv2.imshow('right', rightFrame)
+
+        window_size = 3
+        min_disp = 16
+        num_disp = 112-min_disp
+        stereo = cv2.StereoSGBM_create(minDisparity = min_disp,
+            numDisparities = num_disp,
+            blockSize = 16,
+            P1 = 8*3*window_size**2,
+            P2 = 32*3*window_size**2,
+            disp12MaxDiff = 1,
+            uniquenessRatio = 10,
+            speckleWindowSize = 100,
+            speckleRange = 32
+        )
+
+        print('computing disparity...')
+        disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
+
+        print('generating 3d point cloud...',)
+        h, w = imgL.shape[:2]
+        f = 0.8*w                          # guess for focal length
+        Q = np.float32([[1, 0, 0, -0.5*w],
+                        [0,-1, 0,  0.5*h], # turn points 180 deg around x-axis,
+                        [0, 0, 0,     -f], # so that y-axis looks up
+                        [0, 0, 1,      0]])
+        points = cv2.reprojectImageTo3D(disp, Q)
+        colors = cv2.cvtColor(imgL, cv2.COLOR_BGR2RGB)
+        mask = disp > disp.min()
+        out_points = points[mask]
+        out_colors = colors[mask]
+        out_fn = 'out.ply'
+        write_ply('out.ply', out_points, out_colors)
+        print('%s saved' % 'out.ply')
+
+        #cv2.imshow('right', imgR)
+        #cv2.imshow('left', imgL)
+        cv2.imshow('disparity', (disp-min_disp)/num_disp)
+                
+        #print('finished loop')
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    left.release()
-    right.release()
+    cap.release()
+    
     cv2.destroyAllWindows()
 
-        '''
-        #get image, split and assign to left and right
-        ret, img = cap.read()
-        
-        img1 = img[0:480,  0:640]       #left
-        img2 = img[0:480,  640:1280]   #right
-        #cv2.imshow("Left", img1)
-        #cv2.imshow("Right", img2)
-        singleL=cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-        singleR=cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-        cv2.waitKey()
+'''(_, _, _, _, _, rotationMatrix, translationVector, _, _) = cv2.stereoCalibrate(
+            objectPoints, leftImagePoints, rightImagePoints,
+            leftCameraMatrix, leftDistortionCoefficients,
+            rightCameraMatrix, rightDistortionCoefficients,
+            imageSize, None, None, None, None,
+            cv2.CALIB_FIX_INTRINSIC, TERMINATION_CRITERIA)
 
-        stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
-        disparity = stereo.compute(singleL,singleR)
-        plt.imshow(disparity,'gray')
-        plt.show()
-        cv2.waitKey()
-        cv2.destroyAllWindows()
-        count+=1
-        https://albertarmea.com/post/opencv-stereo-camera/
-    '''
-        
-        
+        (leftRectification, rightRectification, leftProjection, rightProjection,
+            dispartityToDepthMap, leftROI, rightROI) = cv2.stereoRectify(
+                leftCameraMatrix, leftDistortionCoefficients,
+                rightCameraMatrix, rightDistortionCoefficients,
+                imageSize, rotationMatrix, translationVector,
+                None, None, None, None, None,
+                cv2.CALIB_ZERO_DISPARITY, OPTIMIZE_ALPHA)
+
+        leftMapX, leftMapY = cv2.initUndistortRectifyMap(
+            leftCameraMatrix, leftDistortionCoefficients, leftRectification,
+            leftProjection, imageSize, cv2.CV_32FC1)
+        rightMapX, rightMapY = cv2.initUndistortRectifyMap(
+            rightCameraMatrix, rightDistortionCoefficients, rightRectification,
+            rightProjection, imageSize, cv2.CV_32FC1)
+
+        np.savez_compressed(outputFile, imageSize=imageSize,
+        leftMapX=leftMapX, leftMapY=leftMapY, leftROI=leftROI,
+        rightMapX=rightMapX, rightMapY=rightMapY, rightROI=rightROI)
+
+
+'''
