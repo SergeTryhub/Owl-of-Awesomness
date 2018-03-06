2e6bef2f0d6d7262c411e61794082feeac7be84c
omebius <callummoscrip@googlemail.com>
2018-03-06 09:09:25 +0000

    CMV2

--- KeyboardInput.py
+++ KeyboardInput.py
@@ -1,105 +1,85 @@
+# Servo Control
 #!/usr/bin/env python
 # Python 2/3 compatibility
 from __future__ import print_function
 #---------------------
 import socket
+import cv2
 import StringIO
 import time
-import cv2
-import sys, termios, tty, os
+import keyboard
 
 TCP_IP = '10.0.0.10'
 TCP_PORT = 12345
-
 BUFFER_SIZE = 24
- 
-RxC=Rx=1530 
-RyC=Ry=1560
-LxC=Lx=1420
-LyC=Ly=1440 
-NeckC=Neck=1530
-button_delay = 0.2
+#s.close()
 
+RxC=1530 
+RyC=1560
+LxC=1420
+LyC=1440 
+NeckC =1530
+ 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-
-def getch():  #capture the keyboard input
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
- 
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
- 
-print('Control the left camera using WASD and q for capture')
-print('Control the right camera using UJHK and y for capture')
-print('To exit press e') 
-
-
+s.send('{} {} {} {} {}'.format(RxC, RyC, LxC, LyC, NeckC))
+idx=0
 while True:
-
     cap = cv2.VideoCapture('http://10.0.0.10:8080/stream/video.mjpeg')
     ret, img = cap.read()
     cv2.imshow("image", img)
-    cv2.waitKey(300)
+    left = img[0:480,  0:640]
+    right = img[0:480,  641:1280]
+    cv2.waitKey(30)
+    try:        
+        if keyboard.is_pressed('w'):
+            RyC=RyC+10
+            s.send('{} {} {} {} {}'.format(RxC, RyC, LxC, LyC, NeckC))
+            time.sleep(0.05)
+        elif keyboard.is_pressed('a'):
+            RxC=RxC+10
+            s.send('{} {} {} {} {}'.format(RxC, RyC, LxC, LyC, NeckC))
+            time.sleep(0.05)
+        elif keyboard.is_pressed('s'):
+            RyC=RyC-10
+            s.send('{} {} {} {} {}'.format(RxC, RyC, LxC, LyC, NeckC))
+            time.sleep(0.05)
+        elif keyboard.is_pressed('d'):
+            RxC=RxC-10
+            s.send('{} {} {} {} {}'.format(RxC, RyC, LxC, LyC, NeckC))
+            time.sleep(0.05)
+        #######################--THE OTHER SIDE--#########################
+        elif keyboard.is_pressed('up'):
+            LyC=LyC-10
+            s.send('{} {} {} {} {}'.format(RxC, RyC, LxC, LyC, NeckC))
+            time.sleep(0.05)
+        elif keyboard.is_pressed('left'):
+            LxC=LxC+10
+            s.send('{} {} {} {} {}'.format(RxC, RyC, LxC, LyC, NeckC))
+            time.sleep(0.05)
+        elif keyboard.is_pressed('down'):
+            LyC=LyC+10
+            s.send('{} {} {} {} {}'.format(RxC, RyC, LxC, LyC, NeckC))
+            time.sleep(0.05)
+        elif keyboard.is_pressed('right'):
+            LxC=LxC-10
+            s.send('{} {} {} {} {}'.format(RxC, RyC, LxC, LyC, NeckC))
+            time.sleep(0.05)
+        elif keyboard.is_pressed('esc'):
+            cv2.destroyAllWindows()
+            break
+        #######################--IMAGE CAPTURE--#########################
+        elif keyboard.is_pressed('g'):
+            
+            imgName1="K:/OWL/Code/DATA/left_%i.jpg"%idx
+            cv2.imwrite(imgName1, left)
+        
+            imgName2="K:/OWL/Code/DATA/right_%i.jpg"%idx
+            cv2.imwrite(imgName2, right)
+            idx=idx+1
+        else:
+            pass
+    except:
+        break
 
-    char = getch()
-    servo='{} {} {} {} {}'.format(Rx, Ry, Lx, Ly, Neck)
-    #print(servo)
-    s.send(servo)
-    if (char == "w"):
-        #print("Left eye look up")
-        Ly+=10
-        time.sleep(button_delay)
- 
-    elif (char == "s"):
-        #print("Left eye look down")
-        Ly-=10
-        time.sleep(button_delay)
- 
-    elif (char == "a"):
-        #print("Left eye move right")
-        Lx+=10
-        time.sleep(button_delay)
- 
-    elif (char == "d"):
-        #print("Left eye move left")
-        Lx-=10
-        time.sleep(button_delay)
- 
-    elif (char == "q"):
-        print("Capture Left image")
-        time.sleep(button_delay)
-
-    elif (char == "u"):
-        #print("Right eye look up")
-        Ry+=10
-        time.sleep(button_delay)
- 
-    elif (char == "j"):
-        #print("Right eye look down")
-        Ry-=10
-        time.sleep(button_delay)
- 
-    elif (char == "h"):
-        #print("Right eye move right")
-        Rx+=10
-        time.sleep(button_delay)
- 
-    elif (char == "k"):
-        #print("Right eye move left")
-        Rx-=10
-        time.sleep(button_delay)
- 
-    elif (char == "y"):
-        print("Capture Right image")
-        time.sleep(button_delay)
- 
-    elif (char == "e"):
-        print("Exit")
-        cv2.destroyAllWindows()
-        s.close()
-        exit(0)
+print('Bye, have a good day!!')
