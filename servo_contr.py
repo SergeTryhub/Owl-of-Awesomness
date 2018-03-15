058fba06d82b555e18c397c5b9a19bb09f9edc56
SergeTryhub <33597117+SergeTryhub@users.noreply.github.com>
2018-03-15 12:13:10 +0000

    Update servo_contr.py

--- servo_contr.py
+++ servo_contr.py
@@ -14,11 +14,11 @@ TCP_PORT = 12345
 BUFFER_SIZE = 24
 #s.close()
 
-RxC=1530 
+RxC=1460 
 RyC=1560
-LxC=1420
-LyC=1440 
-NeckC =1530
+LxC=1540
+LyC=1470 
+NeckC =1480
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
